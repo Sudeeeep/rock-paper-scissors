9e755a3981f291ab158e933362b7a897a28226ef
Sudeeeep <sudeepn15@gmail.com>
2022-03-21 15:15:23 +0530

    Implemented basic Rock paper scissors game logic in console

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 function computerPlay(){
     const computerOptions = ["rock", "paper", "scissors"] ;
     const computerChoice = computerOptions[(Math.floor(Math.random() * computerOptions.length))];
-    console.log(computerChoice);
+    return computerChoice;
 }
 
+function playGame(player, computer){
+     if ( player == computer){
+         console.log("It's a tie!");
+     }
+     else if ( (player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper") )
+     {
+         console.log("You Win!");
+     }
+     else{
+        console.log("You Lose!");
+     }
+
+}
+
+// Ask player to choose
+const playerSelection = prompt("Rock, Paper, Scissors?");
+
+//computers choice
+const computerSelection = computerPlay();
+console.log("Computer Selected " + computerSelection);
+
+//Function call to play the game
+playGame(playerSelection,computerSelection);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
