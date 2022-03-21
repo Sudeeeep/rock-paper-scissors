a02f64fde859624a0a6f3fbb74c22fc55239b930
Sudeeeep <sudeepn15@gmail.com>
2022-03-21 23:17:15 +0530

    Added tracking scroes

--- script.js
+++ script.js
@@ -4,29 +4,71 @@ function computerPlay(){
     return computerChoice;
 }
 
-function playGame(player, computer){
-     if ( player == computer){
-         console.log("It's a tie!");
+function playGame(player, computer,round){
+  
+     if ( player == computer)
+     {
+        return `Round No. ${round} is a tie`;
+        
      }
      else if ( (player == "rock" && computer == "scissors") || (player == "paper" && computer == "rock") || (player == "scissors" && computer == "paper") )
      {
-         console.log("You Win!");
+         playerCounter++;
+         return `You Win Round No. ${round}! ${player} beats ${computer}` ;
+         
      }
-     else{
-        console.log("You Lose!");
+     else
+     {
+        computerCounter++;
+         return `You Lose Round No. ${round}! ${computer} beats ${player}` ;
+        
      }
 
 }
 
-// Ask player to choose
-const playerSelection = prompt("Rock, Paper, Scissors?");
+function game(){
 
-//computers choice
-const computerSelection = computerPlay();
-console.log("Computer Selected " + computerSelection);
+    let result,i,n;
 
-//Function call to play the game
-playGame(playerSelection,computerSelection);
+    n = prompt("Enter the number of rounds you want to play");
+
+    for ( i = 1; i <=n; i++) 
+    { 
+    // Ask player to choose
+    let playerSelection = prompt("Rock, Paper, Scissors?");
+    playerSelection = playerSelection.toLowerCase();
+
+    //computers choice
+    const computerSelection = computerPlay();
+    console.log(`Computer Selected ${computerSelection}`);
+
+    //Function call to play the game
+     result = playGame(playerSelection, computerSelection,i);
+     console.log(result);
+    }
+
+    // Displaying Final results of the game
+    console.log(`Final Scores: \n Your Score: ${playerCounter} \n Computer's Score: ${computerCounter}`);
+
+    if(playerCounter == computerCounter)
+    {
+        console.log("Tie Game!");
+    }
+    else if (playerCounter > computerCounter)
+    {
+        console.log("CONGRATULATIONS!!!! YOU WON THE GAME!");
+    }
+    else{
+        console.log("Sorry... You lost the game.");
+    }
+ 
+}
+
+
+let playerCounter = 0 ;
+let computerCounter = 0 ;
+
+game();
 
 
 
